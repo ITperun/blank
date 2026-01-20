c095dd579dcde27891519abe8ec4aadeeaad5165
kumihorane <ilyaperun@gmail.com>
2026-01-20 13:46:23 +0100

    uplBUG

---
+++ app/MailSender/MailSender.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MailSender;
+
+use Nette\Application\UI\TemplateFactory;
+use Nette\Mail\Message;
+use Nette\Mail\Mailer;
+
+class MailSender
+{
+    public function __construct(
+        private Mailer $mailer,
+        private TemplateFactory $templateFactory
+    ) {}
+
+    // Метод создания письма
+    public function createNotificationEmail(string $recipient, string $name, string $item, string $note): Message
+    {
+        // Создаем шаблон
+        $template = $this->templateFactory->createTemplate();
+        $template->setFile(__DIR__ . '/email.latte');
+        
+        // Передача в шаблон
+        $template->name = $name;
+        $template->item = $item;
+        $template->note = $note;
+
+        // Создаем объект сообщения
+        $mail = new Message;
+        $mail->setFrom('rucickaigor@seznam.cz', 'E-shop MOP'); // Укажи здесь тот же email, что в конфиге
+        $mail->addTo($recipient);
+        $mail->setSubject('Nová objednávka: ' . $item);
+        $mail->setHtmlBody($template);
+
+        return $mail;
+    }
+
+    // Универсальный метод отправки
+    public function sendEmail(Message $mail): void
+    {
+        try {
+            $this->mailer->send($mail);
+        } catch (\Exception $e) {
+            // Логируем ошибку или выбрасываем дальше, если нужно
+            // Tracy\Debugger::log($e);
+        }
+    }
+}
--- app/Presentation/Front/Home/HomePresenter.php
+++ app/Presentation/Front/Home/HomePresenter.php
@@ -3,13 +3,34 @@
 declare(strict_types=1);
 
 namespace App\Presentation\Front\Home;
-use Nette\Application\UI\Presenter;
 
+use Nette\Application\UI\Presenter;
+use App\MailSender\MailSender;
 
 final class HomePresenter extends Presenter
 {
+    public function __construct(
+        private MailSender $mailSender
+    ) {
+        parent::__construct();
+    }
+
     public function renderDefault(): void
     {
         $this->template->title = 'Moje (DEV) MOP';
+    }
+    public function handleSendEmail(): void
+    {
+        $mail = $this->mailSender->createNotificationEmail(
+            "zakaznik@seznam.cz",
+            "Martin Kokes", 
+            "Autíčko Burago Hyundai IONIQ5", 
+            "Prosím o zelenou barvu"
+        );
+
+        $this->mailSender->sendEmail($mail);
+
+        $this->flashMessage('Email byl úspěšně odeslán!', 'success');
+        $this->redirect('this');
     }
 }
